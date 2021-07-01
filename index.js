74336e7354eeadc052d2ff627e68af7b4027352f
Silas Silva <silvasilas66@gmail.com>
2021-07-01 11:50:37 -0300

    Setting up DataBase

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const morgan = require('morgan');
 
 dotenv.config();
 
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
+    console.log('Connected to MongoDB sucessfully');
+});
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Server is running on port port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Server is running on port port ${port}!`));
