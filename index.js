95429243c42931c8385e9a287ce1c0cdbdc855c2
Silas Silva <silvasilas66@gmail.com>
2021-07-01 13:02:31 -0300

    Setting middlewares

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
     console.log('Connected to MongoDB sucessfully');
 });
 
+// Middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`Server is running on port port ${port}!`));
