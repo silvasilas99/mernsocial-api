461bb43d4c0cfe97530c4ca47b030c058da91a09
Silas Silva <silvasilas66@gmail.com>
2021-07-09 18:12:56 -0300

    Posts CRUD

--- controllers/PostsController.js
+++ controllers/PostsController.js
@@ -1,7 +1,50 @@
 const Post = require('./../models/Post');
 
 module.exports = {
-    async hello (req, res) {
-    	return res.status(200).json({ message: "Welcome, user" });
-    }
+    async createPost (req, res) {
+    	const newPost = new Post(req.body);
+	  	try {
+	    	const savedPost = await newPost.save();
+	    	res.status(200).json(savedPost);
+	  	} catch (err) {
+	    	return res.status(500).json(err.message);
+	  	}
+    },
+
+    async deletePost (req, res) {
+    	try {
+    		const post = await Post.findById(req.params.id);
+    		if (post.userId === req.body.userId) {
+      			await post.deleteOne();
+      			res.status(200).json({ message: "Post deleted successfully" });
+    		} else {
+      			res.status(403).json({ message: "You can delete only a post that you created" });
+    		}
+  		} catch (err) {
+    		return res.status(500).json(err.message);
+  		}
+    },
+
+    async updatePost (req, res) {
+  		try {
+    		const post = await Post.findById(req.params.id);
+    		if (post.userId === req.body.userId) {
+      			await post.updateOne({ $set: req.body });
+      			res.status(200).json({ message: "Post updated successfully" });
+    		} else {
+      			res.status(403).json({ message: "You can update only a post that you created" });
+    		}
+  		} catch (err) {
+    		return res.status(500).json(err.message);
+  		}
+    },
+
+    async getSinglePost (req, res) {
+    	try {
+    		const post = await Post.findById(req.params.id);
+    		res.status(200).json(post);
+  		} catch (err) {
+    		return res.status(500).json(err.message);
+  		}
+    },
 };
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const PostsController = require('./../controllers/PostsController');
 
-router.get('/', PostsController.hello);
+router.post('/', PostsController.createPost);
+router.delete('/:id', PostsController.deletePost);
+router.put('/:id', PostsController.updatePost);
+router.get('/:id', PostsController.getSinglePost);
 
 module.exports = router;
